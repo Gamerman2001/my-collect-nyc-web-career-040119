d2eb080dcf38036f313f2486a57a1067d24cd8ec
Troy Gooden <tro12185@gmail.com>
2019-03-21 19:26:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+      i = 0 
+      upper = []
+      
+      while i < array.length 
+        upper.push(yield array[i])
+        i += 1 
+      end
+      upper
+  end
+end  
